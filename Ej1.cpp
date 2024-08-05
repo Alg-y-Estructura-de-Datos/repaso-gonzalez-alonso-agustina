9d2c8a0a28ba358c814c3c11e1374f965e53ab3b
agus gonzalez <2313442@ucc.edu.ar>
2024-08-05 12:06:28 -0300

    se agregan ej

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,37 @@
+
+/*Ej1: Imagina que tienes dos vasos, uno con jugo de naranja y otro con jugo de manzana.
+Queremos escribir un programa en C++ que tenga una función llamada
+intercambiarJugos que tome dos vasos (representados como variables) y los
+intercambie. Luego, en la función main, pedimos al usuario que ingrese la cantidad
+de jugo en cada vaso, llamamos a la función intercambiarJugos y mostramos las
+cantidades después del intercambio.
+*/
+
 #include <iostream>
 using namespace std;
 
-int main() {
+void intercambiarJugos (double &vaso1, double &vaso2) {
+double aux;
+  aux=vaso1;
+  vaso1=vaso2;
+  vaso2=aux;
 
-    cout << "Examen Final Prog 3" << endl;
+}
+
+int main() {
+    cout << "Ejercicio 1: " << endl;
+
+    double vaso1, vaso2;
+
+    cout << "Ingrese la cantidad de jugo en el vaso 1" << endl;
+    cin >> vaso1;
+
+    cout << "Ingrese la cantidad de jugo en el vaso 2" << endl;
+    cin >> vaso2;
+
+    intercambiarJugos(vaso1,vaso2);
+    cout << "vaso 1: " << vaso1 << endl;
+    cout << "vaso 2: " << vaso2 << endl;
 
     return 0;
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,25 @@
+
+/*Escribe un programa en C++ que calcule el factorial de un número entero positivo
+usando un bucle.
+*/
+
 #include <iostream>
 using namespace std;
 
+unsigned long long fact (int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
 int main() {
+    cout << "Ejercicio 2" << endl;
+    int n;
 
-    cout << "Examen Final Prog 3" << endl;
+    cout << "Ingrese un numero" << endl;
+    cin >> n;
+
+    cout << "El factorial de " << n << " es: " << fact(n);
 
     return 0;
 }
